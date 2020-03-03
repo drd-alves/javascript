4d1e607eb01388ae96e7400966e0e94b0a4f0c53
EDUARDO ALVES RODRIGUES <eduardo.arodrigues10@senacsp.edu.br>
2020-03-03 12:23:13 -0300

    Exercicios 03/03

---
+++ exercicios_03-03/assets/js/ex2.js
@@ -0,0 +1,16 @@
+function atualizaNome(){
+
+    var nometrocado = prompt("Digite seu nome:")
+    // var nome = document.getElementById("nome").textContent
+    // var nome = nome.split(",").join(""); nome = nome.split(" ")
+    // var nometrocado = prompt("Digite seu nome: ") 
+
+    // for(i=0; i<nome.length; i++){
+    //     nome[i] = nome[i].replace(nome[1],nometrocado)
+    // }
+
+    nome.textContent = "Olá " + nometrocado + ", como vai?" 
+    
+    // console.log(nome)
+
+} 
